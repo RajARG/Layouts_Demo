44b9d562fa29c5a856fba1edae39ea89c75cae7a
1707592 <gupta.amey@gmail.com>
2020-02-26 14:15:45 -0500

    2/26/2020

--- app/src/main/java/com/guptaamey/layoutsdemo/MainActivity.java
+++ app/src/main/java/com/guptaamey/layoutsdemo/MainActivity.java
@@ -1,14 +1,25 @@
 package com.guptaamey.layoutsdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout a;
+    Button switch_layouts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        a = R.layout.activity_main;
+
+
     }
 }
